c7d0893cab20d1bbb2b021f20eff15978c5382b9
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2025-03-20 00:08:46 -0300

    correções e sofrimento

--- src/main/java/com/whispe2/whispe2/controller/usuarioController.java
+++ src/main/java/com/whispe2/whispe2/controller/usuarioController.java
@@ -1,22 +1,37 @@
 package com.whispe2.whispe2.controller;
 
-import java.util.List;
 import java.util.ArrayList;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Valid;
+import jakarta.validation.Validator;
 import com.whispe2.whispe2.model.usuario;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
-public class usuarioController {
+public class UsuarioController {
+
+    @Autowired
+    private Validator validator;
 
     @GetMapping("/show")
-    public List<usuario> getusuario() {
+    public ResponseEntity<List<usuario>> getUsuarios() {
         List<usuario> usuarios = new ArrayList<>();
-        usuarios.add(new usuario(1, "teste"));
-        usuarios.add(new usuario(2, "teste2"));
-        usuarios.add(new usuario(3, "teste3"));
-        return usuarios;
+        usuarios.add(new usuario(1, "João", "123.456.789-00"));
+        usuarios.add(new usuario(2, "Maria", "987.654.321-00"));
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<?> insertUsuario(@Valid @RequestBody usuario usuario) {
+        Set<ConstraintViolation<usuario>> violations = validator.validate(usuario);
+        System.out.println("Usuário inserido: " + usuario);
+        return ResponseEntity.ok(usuario);
     }
 }
--- src/main/java/com/whispe2/whispe2/model/usuario.java
+++ src/main/java/com/whispe2/whispe2/model/usuario.java
@@ -1,21 +1,32 @@
 package com.whispe2.whispe2.model;
 
+import org.hibernate.validator.constraints.br.CPF;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class usuario {
 
-    @NotNull
-    @NotBlank
+    @NotNull (message = "Id não pode ser nulo")
+    @NotBlank (message = "Id não pode ser vazio")
     private Integer id;
 
-    @NotNull
-    @NotBlank
+    @NotNull(message = "Nome não pode ser nulo")
+    @NotBlank(message = "Nome não pode ser vazio")
     private String nome;
 
-    public usuario(Integer id, String nome) {
+    @CPF (message = "CPF inválido")
+    @NotNull (message = "CPF não pode ser nulo")
+    @NotBlank (message = "CPF não pode ser branco")
+    private String cpf;
+
+    public usuario(Integer id, String nome, String cpf) {
         this.id = id;
         this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
     }
 
     public Integer getId() {
@@ -32,5 +43,9 @@ public class usuario {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 }
